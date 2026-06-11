b2bb963dc45fc861c99cf4a1d1971aebe9200152
davidsonjean210-lab <davidsonjean210@gmail.com>
2026-06-10 22:48:34 -0400

    Actualizar BERIS_HUB_ULTRA.lua

--- BERIS_HUB_ULTRA.lua
+++ BERIS_HUB_ULTRA.lua
@@ -13,8 +13,8 @@ local incomeMode = "BASICO"
 
 local config = {
     BASICO = {money = 100, delay = 2},
-    PREMIUM = {money = 10000, delay = 1.5},
-    PLUS = {money = 20000, delay = 1}
+    PREMIUM = {money = 100000, delay = 1.5},
+    PLUS = {money = 20000000, delay = 1}
 }
 
 ------------------------------------------------
